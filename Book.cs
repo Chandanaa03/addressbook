48282512e699d9af7c76629117b74a14ee2859dd
chandana Reddy <chandanareshma01@gmail.com>
2022-04-02 13:10:11 +0530

    Contact

--- Book.cs
+++ Book.cs
@@ -15,29 +15,29 @@ namespace AddressBook
             contactList = new List<Contact>();
         }
 
-        public void addContacts(string fistName, string lastName, string address, string city, string state, int zip, long phoneNumber, string email)
+        public void addContacts(string FirstName, string LastName, string Address, string City, string State, int zipcode, long PhoneNumber, string Email)
         {
-            //Create object of Contact class
+            
             Contact contact = new Contact();
-            //calling Varivale using object
-            contact.FirstName = fistName;
-            contact.LastName = lastName;
-            contact.Address = address;
-            contact.City = city;
-            contact.State = state;
-            contact.zipcode = zip;
-            contact.PhoneNumber = phoneNumber;
-            contact.Email = email;
-            //adding contact details in contact list 
+            
+            contact.FirstName = FirstName;
+            contact.LastName = LastName;
+            contact.Address = Address;
+            contact.City = City;
+            contact.State = State;
+            contact.zipcode = zipcode;
+            contact.PhoneNumber = PhoneNumber;
+            contact.Email = Email;
+            
             contactList.Add(contact);
         }
-        //method print
+       
         public void print()
         {
-            //using foreach loop for calling the variable 
+           
             foreach (Contact contact in contactList)
             {
-                //Prinitng the Op
+               
                 Console.WriteLine("FirstName: " + contact.FirstName);
                 Console.WriteLine("LastName: " + contact.LastName);
                 Console.WriteLine("Address: " + contact.Address);
