b3d6d0978397fe50cfa301f0b99e72012c6558dc
chandana Reddy <chandanareshma01@gmail.com>
2022-04-02 13:07:33 +0530

    contacts

--- Program.cs
+++ Program.cs
@@ -6,13 +6,40 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("------Welcome to Address Book system------");
             
+            Console.WriteLine("Welcome to Address Book Program");
+
             Book addressBook = new Book();
-           
-            addressBook.addContacts("Chandana", "Reddy", "Nehrunagr", "Guntur", "AP", 522001, 5263893371, "chandanareshma@gmail.com");
+
+            AddToContacts(addressBook);
+            AddToContacts(addressBook);
+
             addressBook.print();
             Console.ReadLine();
         }
+        //adding contacts
+        public static  void AddToContacts(Book addressBook)
+        {
+            Console.WriteLine("Enter FirstName");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine("Enter LastName");
+            string LastName = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            string Address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            string City = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            string State = Console.ReadLine();
+            Console.WriteLine("Enter Zipcode");
+            int Zipcode = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter PhoneNumber");
+            long PhoneNumber = long.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Email id");
+            string Email = Console.ReadLine();
+            addressBook.addContacts(FirstName, LastName, Address, City, State, Zipcode, PhoneNumber, Email);
+            Console.ReadLine();
+
+        }
     }
 }
+    
