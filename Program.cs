31c45db3be74da76f7b10d91ea18a0da112bafd8
chandana Reddy <chandanareshma01@gmail.com>
2022-04-02 12:48:07 +0530

    contacts

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------Welcome to Address Book system------");
+            
+            Book addressBook = new Book();
+           
+            addressBook.addContacts("Chandana", "Reddy", "Nehrunagr", "Guntur", "AP", 522001, 5263893371, "chandanareshma@gmail.com");
+            addressBook.print();
+            Console.ReadLine();
+        }
+    }
+}
